89e65d71a4e199928c29700bc65b6912638cbff7
Bailey Lockheart <baileylockheart@gmail.com>
2024-12-28 17:03:53 -0800

    Add files via upload

---
+++ public/javascript/navbar.js
@@ -0,0 +1,37 @@
+document.getElementById("nav").innerHTML = `
+<div>
+    <ul>
+        <li><a href="/about.html">About</a></li>
+        <li><a href="/collections.html">Collections</a></li>
+        <li><a href="/blog.html">Blog</a></li>
+        <li><a href="/links.html">Links</a></li>
+    </ul>
+</div>
+<style>
+#nav {
+    background-color: var(--light);
+    width: 100%;
+    padding-top: 1px;
+    padding-bottom: 1px;
+    margin-bottom: 10px;
+    color:var(--main);
+    font-family: var(--serif);
+    font-size: 1.25rem;
+}
+#nav ul {
+    display:flex;
+    list-style-type:none;
+    justify-content: space-evenly;
+  }
+#nav a:hover{
+    color: var(--bg);
+    transition: 0.6s ease;
+    -webkit-transition: 0.6s ease
+}
+@media (max-width: 768px){
+    #nav ul{
+        margin-left: -2.75rem;
+    }
+}
+</style>
+`;
