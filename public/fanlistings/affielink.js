b8a42dcb5fc87b6884c9ed6011844697675da14e
Bailey <baileylockheart@gmail.com>
2025-02-21 16:57:07 -0800

    Officially at 125 listings... what

--- public/fanlistings/affielink.js
+++ public/fanlistings/affielink.js
@@ -1,10 +1,10 @@
 var tag = document.getElementById("affielink");
 
 var imageUrls = [
-    "img/links/75x50-amour.png"
-  , "img/links/precious.png"
-  , "img/links/shadowlord.png"
-  , "img/links/ww-affie.png"
+    "https://file.garden/ZrZSgsrYfQXsO7QH/affielink/amour.png"
+  , "https://file.garden/ZrZSgsrYfQXsO7QH/affielink/precious.png"
+  , "https://file.garden/ZrZSgsrYfQXsO7QH/affielink/shadowlord.png"
+  , "https://file.garden/ZrZSgsrYfQXsO7QH/ww/ww-affie.png"
 ];
 var imageLinks = [
     "https://love.suga.nu/"
@@ -15,14 +15,14 @@ var imageLinks = [
 
 function getImageHtmlCode() {
  var dataIndex = Math.floor(Math.random() * imageUrls.length);
- var img = '<a href=\"' + imageLinks[dataIndex] + '"><img src="';        
+ var img = '<a href=\"' + imageLinks[dataIndex] + '" target=\"_blank\"><img src="';        
  img += imageUrls[dataIndex];
  img += '\" alt=\"AffieLink\"/></a>';
  return img;
 }
 
 tag.insertAdjacentHTML('afterbegin', `
-    <div style="height:15px;margin:5px"><a href="links.html" title="AffieLink"><img src="img/affielink.png" style="border:0;margin:0;height:15px" alt="AffieLink" /></a></div>
+    <div style="height:15px;margin:5px"><a href="links.html" title="AffieLink"><img src="https://file.garden/ZrZSgsrYfQXsO7QH/affielink/affielink.png" style="border:0;margin:0;height:15px" alt="AffieLink" /></a></div>
 `);
 
 document.write(getImageHtmlCode());
